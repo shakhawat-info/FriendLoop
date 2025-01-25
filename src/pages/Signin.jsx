dfb41b5b9e55c6d4357135a5eaa6c1002df49d70
MD. SHAKHAWAT HOSSAIN <inform.shakhawat@gmail.com>
2025-01-25 22:20:04 +0600

    Login/Signup

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import { getAuth, signInWithPopup, GoogleAuthProvider , signInWithEmailAndPassword} from "firebase/auth";
+import { getDatabase, ref, set } from "firebase/database";
 
 // components
 import { Button, Checkbox, FormControlLabel, TextField } from "@mui/material";
 import { Link } from "react-router";
+import Warning from "../components/Warning";
+
 
 // icons
 import { GoEye } from "react-icons/go";
@@ -11,13 +15,83 @@ import { AiOutlineLogin } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebookF } from "react-icons/fa";
 import { SiGithub } from "react-icons/si";
+import { useRef } from "react";
 
 const Signin = () => {
-
+  const db = getDatabase();
+  const auth = getAuth();
   const [showPassword, setShowPassword] = useState(false);
+  const provider = new GoogleAuthProvider();
+  const [warning , setWarning] = useState('');
 
+
+  // email func
+  let [email , setEmail] = useState('')
+  let emailSet = (e)=>{
+    setEmail(e.target.value)
+  }
+
+  // password func
+  let [password , setPassword] = useState('')
+  let passSet = (e)=>{
+    setPassword(e.target.value)
+  }
+
+  // SignIn with username
+  let remembeRref = useRef()
+  let SignIn = ()=>{
+    // console.log(remembeRref.current.firstElementChild.checked);
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      // ...
+      console.log(user);
+    // set data to database
+    set(ref(db, 'users/' + user.uid), {
+      name: user.displayName,
+      email: user.email,
+      uid: user.uid ,
+      metadata: {...user.metadata},
+      photoURL: user.photoURL
+    });
+      
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+  }
+
+  // google login
+  let googleLogin = ()=>{
+    signInWithPopup(auth, provider)
+  // .then((result) => {
+  //   const user = result.user;
+  //   // console.log(user);
+    
+  //   // set data to database
+  //   set(ref(db, 'users/' + user.uid), {
+  //     name: user.displayName,
+  //     email: user.email,
+  //     uid: user.uid ,
+  //     metadata: {...user.metadata},
+  //     photoURL: user.photoURL
+  //   });
+    
+    
+  // }).catch((error) => {
+  //   // Handle Errors here.
+  //   const errorCode = error.code;
+  //   const errorMessage = error.message;
+  //   // The AuthCredential type that was used.
+  //   const credential = GoogleAuthProvider.credentialFromError(error);
+  //   setWarning(errorCode);
+  // });
+  }
   return (
-    <div className="  ">
+    <div className=" relative ">
+      <Warning er={warning}  />
       <div className="flex flex-col justify-center h-screen w-2/5 mx-auto   ">
         {/* welcome */}
         <div className="">
@@ -31,11 +105,11 @@ const Signin = () => {
 
         {/* UserName */}
         <div className="mt-10">
-          <TextField id="userName" label="User Name" variant="outlined" sx={{".MuiOutlinedInput-input":{padding: '10px'} , ".css-19qnlrw-MuiFormLabel-root-MuiInputLabel-root":{top: '-6px'} , ".MuiInputBase-root input": {color: 'gray'}}} className="w-full    " placeholder="UserName or Email"/>
+          <TextField onChange={emailSet} id="userName" label="User Name" variant="outlined" sx={{".MuiOutlinedInput-input":{padding: '10px'} , ".css-19qnlrw-MuiFormLabel-root-MuiInputLabel-root":{top: '-6px'} , ".MuiInputBase-root input": {color: 'gray'}}} className="w-full    " placeholder="UserName or Email"/>
         </div>
         {/* Password */}
         <div className="mt-5 relative">
-          <TextField type={showPassword ? "text" : "password"} id="password" label="Password" variant="outlined" sx={{".MuiInputBase-root input":{padding: '10px'}, ".css-19qnlrw-MuiFormLabel-root-MuiInputLabel-root " : {top: '-6px'}}} className="w-full    "   placeholder="password"/>
+          <TextField onChange={passSet} type={showPassword ? "text" : "password"} id="password" label="Password" variant="outlined" sx={{".MuiInputBase-root input":{padding: '10px'}, ".css-19qnlrw-MuiFormLabel-root-MuiInputLabel-root " : {top: '-6px'}}} className="w-full    "   placeholder="password"/>
           { showPassword ?  
           < GoEye  className="absolute top-[4px] right-[0px] text-xl cursor-pointer p-2 box-content   " onClick={()=> setShowPassword ( prevState => !prevState)}/>
           : 
@@ -45,21 +119,20 @@ const Signin = () => {
 
         {/* remember/forget */}
         <div className="flex items-center justify-between  ">
-          <FormControlLabel control={<Checkbox defaultChecked />} label="Remember me" />
+          <FormControlLabel   control={<Checkbox defaultChecked ref={remembeRref}/>} label="Remember me" />
           <Link to="" className="EffUnderline relative text-brand font-Ubuntu text-lg   ">Forget Password?</Link>
         </div>
 
         {/* signIN Button */}
-        <div className="">
-          <span className="loader"></span>
-          <Button variant="outlined" endIcon={<AiOutlineLogin />}> SignIn </Button>
+        <div className="flex justify-end mt-5">
+          <Button onClick={SignIn}  variant="outlined" endIcon={<AiOutlineLogin />}> SignIn </Button>
         </div>
 
         {/* SignIn with */}
         <div className="mt-[100px]">
           <h3 className="font-Aldrich text-lg text-center  ">SignIn with</h3>
           <div className="flex justify-center gap-x-5 mt-5">
-            <Button variant="outlined" endIcon={<FcGoogle />}> Google </Button>
+            <Button onClick={googleLogin} variant="outlined" endIcon={<FcGoogle />}> Google </Button>
             <Button variant="outlined" endIcon={<FaFacebookF />}> Facebook </Button>
             <Button variant="outlined" endIcon={<SiGithub  className="text-black"/>}> Facebook </Button>
           </div>
